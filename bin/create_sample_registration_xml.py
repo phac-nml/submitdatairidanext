a5ededee594095541c1f318a5ecffc4b174921c3
Dan Fornika <dfornika@gmail.com>
2025-03-07 15:38:55 -0800

    Add sample registration xml creation script (#8)
    * Correct the version in manifest
    
    * Revert "Correct the version in manifest"
    
    This reverts commit 50835036723f69df03da77f75e6d86e389efdc38.
    
    * Rename pipeline (#2) (#3)
    
    * start renaming pipeline
    
    * Add irida next plugin
    
    * Rename references
    
    * more renaming
    
    * revert naming to make tests pass
    
    * fix schema url
    
    * Fix linting tests
    
    * Add nf-core version to .nf-core.yml
    
    * fixing pre-commit
    
    * Update CHANGELOG
    
    * Get all tests & linting to pass (main) (#6)
    
    * Rename pipeline (#2)
    
    * start renaming pipeline
    
    * Add irida next plugin
    
    * Rename references
    
    * more renaming
    
    * revert naming to make tests pass
    
    * fix schema url
    
    * Fix linting tests
    
    * Add nf-core version to .nf-core.yml
    
    * fixing pre-commit
    
    * Update CHANGELOG
    
    * Get tests & linting passing (dev) (#5)
    
    * Correct the version in manifest
    
    * Revert "Correct the version in manifest"
    
    This reverts commit 50835036723f69df03da77f75e6d86e389efdc38.
    
    * Rename pipeline (#2) (#3)
    
    * start renaming pipeline
    
    * Add irida next plugin
    
    * Rename references
    
    * more renaming
    
    * revert naming to make tests pass
    
    * fix schema url
    
    * Fix linting tests
    
    * Add nf-core version to .nf-core.yml
    
    * fixing pre-commit
    
    * Update CHANGELOG
    
    * exclude resource-related params from linting
    
    * Add status badges
    
    ---------
    
    Co-authored-by: Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
    Co-authored-by: Kyla Cochrane <Kyla.Cochrane@phac-aspc.gc.ca>
    Co-authored-by: Steven Sutcliffe <47899883+sgsutcliffe@users.noreply.github.com>
    Co-authored-by: Steven Sutcliffe <sgsutcliffe@gmail.com>
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
    Co-authored-by: Kyla Cochrane <Kyla.Cochrane@phac-aspc.gc.ca>
    Co-authored-by: Steven Sutcliffe <47899883+sgsutcliffe@users.noreply.github.com>
    Co-authored-by: Steven Sutcliffe <sgsutcliffe@gmail.com>
    
    * Add sample registration xml creation script
    
    * Fix linting issues
    
    ---------
    
    Co-authored-by: Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
    Co-authored-by: Kyla Cochrane <Kyla.Cochrane@phac-aspc.gc.ca>
    Co-authored-by: Steven Sutcliffe <47899883+sgsutcliffe@users.noreply.github.com>
    Co-authored-by: Steven Sutcliffe <sgsutcliffe@gmail.com>

---
+++ bin/create_sample_registration_xml.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+"""Create a Sample Registration XML file."""
+
+import argparse
+import csv
+import json
+import logging
+import sys
+
+import xml.etree.ElementTree as ET
+
+from pathlib import Path
+from typing import Optional
+
+logger = logging.getLogger()
+
+def parse_input(input_path: Path):
+    """
+    Parse input CSV file and return a list of dictionaries.
+
+    :param input_path: Path to the CSV file.
+    :type input_path: Path
+    :return: List of dictionaries representing each row in the CSV.
+    :rtype: list[dict[str, str]]
+    """
+    parsed_input = []
+    with open(input_path, newline='', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            parsed_input.append(row)
+
+    return parsed_input
+
+def convert_to_xml(sample_metadata: dict, fieldname_mapping: dict):
+    """
+    Convert a CSV file to an XML file in the specified format.
+
+    :param sample_metadata: Parsed sample metadata.
+    :type sample_metadata: list[dict]
+    :param fieldname_mapping: Mapping of XML element tags to sample metadata fieldnames.
+    :type fieldname_mapping: dict[str, str]
+    :return: The sample registration XML element tree
+    :rtype: ET.ElementTree
+    """
+    root = ET.Element("SAMPLE_SET")
+
+    for row in sample_metadata:
+        alias_fieldname = fieldname_mapping['sample_alias']
+        sample = ET.SubElement(root, "SAMPLE", alias=row[alias_fieldname])
+
+        taxon_id_fieldname = fieldname_mapping['taxon_id']
+        sample_name = ET.SubElement(sample, "SAMPLE_NAME")
+        ET.SubElement(sample_name, "TAXON_ID").text = row[taxon_id_fieldname]
+
+        sample_attribute_mapping = {
+            "collection_date": "collection date",
+            "geographic_location_country": "geographic location (country and/or sea)",
+        }
+
+        attributes = ET.SubElement(sample, "SAMPLE_ATTRIBUTES")
+        for attribute_fieldname, attribute_tag in sample_attribute_mapping.items():
+            attribute = ET.SubElement(attributes, "SAMPLE_ATTRIBUTE")
+            ET.SubElement(attribute, "TAG").text = attribute_tag
+            ET.SubElement(attribute, "VALUE").text = row[attribute_fieldname]
+
+    tree = ET.ElementTree(root)
+
+    return tree
+
+def write_output_xml(output_xml_tree: ET.ElementTree, output_xml_path: Optional[Path]):
+    """
+    Write output XML to file (or stdout if no output path provided)
+    :param output_xml_tree: XML data to write
+    :type output_xml_tree: ET.ElementTree
+    :param output_xml_path: Path to write XML file (or print to stdout if None)
+    :type output_xml_path: Optional[Path]
+    :return: None
+    :rtype: None
+    """
+    ET.indent(output_xml_tree, space="  ", level=0)
+    if output_xml_path:
+        output_xml_tree.write(output_xml_path, encoding="utf-8", xml_declaration=True)
+    else:
+        sys.stdout.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+        ET.dump(output_xml_tree)
+
+
+def main(args):
+    fieldname_mapping = {
+        "sample_alias": args.sample_alias_fieldname,
+        "taxon_id": args.taxon_id_fieldname,
+        "collection_date": args.collection_date_fieldname,
+        "geographic_location_country": args.geographic_location_country_fieldname,
+    }
+    input_sample_metadata = parse_input(args.input)
+    sample_submission_xml_data = convert_to_xml(input_sample_metadata, fieldname_mapping)
+
+    write_output_xml(sample_submission_xml_data, args.output)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', type=Path, help="Input Sample Metadata File.")
+    parser.add_argument('--sample-alias-fieldname', type=str, default="sample", help="Fieldname associated with sample alias in input file.")
+    parser.add_argument('--taxon-id-fieldname', type=str, default="taxon_id", help="Fieldname associated with sample taxon ID in input file.")
+    parser.add_argument('--collection-date-fieldname', type=str, default="collection_date", help="Fieldname associated with sample collection date in input file.")
+    parser.add_argument('--geographic-location-country-fieldname', type=str, default="geographic_location_country", help="Fieldname associated with sample collection geographic location (country) in input file.")
+    parser.add_argument('-o', '--output', type=Path, help="Output Sample Submission XML File.")
+    args = parser.parse_args()
+    main(args)
