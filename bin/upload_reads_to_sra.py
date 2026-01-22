d8eec78595e9b3768fe16fab71b0f8d60f7eb439
Dan Fornika <dfornika@gmail.com>
2026-01-22 14:31:05 -0800

    Enable passive mode for FTP connection

--- bin/upload_reads_to_sra.py
+++ bin/upload_reads_to_sra.py
@@ -77,6 +77,7 @@ def connect_and_login(username: str, password: str, server: str, remote_path: Pa
     ftp = None
     try:
         ftp = ftplib.FTP(server, user=username, passwd=password)
+        ftp.set_pasv(True)
     except ConnectionRefusedError as e:
         logger.error(f"Could not connect to {server} as {username}")
         exit(1)
